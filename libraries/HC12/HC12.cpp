52e87d9127168f41d7749b1bba522d5f01636d33
Monoxide13 <git@monoxide13.com>
2019-07-22 20:15:07 -0500

    Updated HC12 lib for USB debugging. Needs more work.

--- libraries/HC12/HC12.cpp
+++ libraries/HC12/HC12.cpp
@@ -1,59 +1,83 @@
-
-
 #include "HC12.hpp"
 
-HC12::HC12(Stream* serialIn, unsigned short commandPinIn) : serialIn(serial) : commandPinIn(commandPin) {
+HC12::HC12(HardwareSerial* serialIn, unsigned short commandPinIn) : serial(serialIn), commandPin(commandPinIn) {
 	HC12ReadBuffer.reserve(32);
+	lastRead.reserve(8);
 };
 
 bool HC12::init(){
-	serial->start(9600);
+	serial->begin(9600);
+	HC12ReadBuffer = "";                     // Read/Write Buffer 1 for HC12
+	SerialReadBuffer = "";                   // Read/Write Buffer 2 for Serial
+	SerialEnd = false;                      // Flag to indicate End of Serial String
+	HC12End = false;                        // Flag to indiacte End of HC12 String
+	commandMode = false;                    // Send AT commands
+	lastHeartbeat=0;
+	lastRead="";
+	newStage=0;
 };
 
-void HC12::sendCommand(char * ptr, unsigned int length){
+void HC12::sendCommand(String command){
+	serial->println(command);
 	digitalWrite(commandPin, LOW);
 	delay(200);
-	serial.write();
+	serial->print(command);
 	delay(200);
 	digitalWrite(commandPin, HIGH);
 };
-bool HC12::send(	 input){
-	serial.println(input);
+bool HC12::send(String input){
+	serial->println(input);
+	
 };
 unsigned int HC12::receive(){
-	retVal=0;
-	if (serial.available()) {
-    while (serial.available()) {
-      HC12ByteIn = seral.read();
-      if (HC12ByteIn == '\n')                   // At the end of the line
-        HC12End = true;                           // Set HC12End flag to true
-      else
-        HC12ReadBuffer += char(HC12ByteIn);         // Write each character of byteIn to HC12ReadBuffer
-    }
-    if (HC12End) {                                // If HC12End flag is true
-      if (HC12ReadBuffer.startsWith("AT")) {      // Check to see if a command is received from remote
-        digitalWrite(HC12SetPin, LOW);            // Enter command mode
-        delay(100);                               // Delay before sending command
-        Serial.print(SerialReadBuffer);           // Echo command to serial if connected.
-        delay(500);                               // Wait 0.5 s for reply
-        digitalWrite(HC12SetPin, HIGH);           // Exit command / enter transparent mode
-        delay(100);                               // Delay before proceeding
-        Serial1.println("Remote Command Executed");  // Acknowledge execution
-        while (Serial1.available()) {
-          Serial1.write(Serial1.read());
-        }
-      } else {
-		  // It wasn't a local command. Set it as the ruturn value;
-      }
-      HC12ReadBuffer = "";                        // Empty buffer
-      HC12End = false;                            // Reset flag
-    }
-  }
-  return retVal;
+	int retVal=0;
+	while (serial->available()) {
+	  HC12ByteIn = serial->read();
+	  if (HC12ByteIn == '\n'){                   // At the end of the line
+		  if (HC12ReadBuffer.startsWith("AT")) {      // Check to see if a command is received from remote
+			digitalWrite(commandPin, LOW);            // Enter command mode
+			delay(100);                               // Delay before sending command
+			serial->print(SerialReadBuffer);           // Echo command to HC12.
+			delay(200);                               // Wait 0.2 s for reply
+			digitalWrite(commandPin, HIGH);           // Exit command / enter transparent mode
+			delay(100);                               // Delay before proceeding
+			serial->println("Remote Command Executed");  // Acknowledge execution
+			delay(10);
+		  } else {
+			  // It wasn't a local command. Set it as the ruturn value;
+			  if(HC12ReadBuffer.length()>7){
+				  HC12ReadBuffer="";
+				  serial->println("BDTBDT");
+			  }else{
+				lastRead=HC12ReadBuffer;
+			  }
+		  }
+		  HC12ReadBuffer = "";                        // Empty buffer
+	  }
+	  else
+		HC12ReadBuffer += char(HC12ByteIn);         // Write each character of byteIn to HC12ReadBuffer
+	}
+	return retVal;
 };
 bool HC12::changeBaud(unsigned int in){
+	char command[16];
+	switch (in){
+		case 2400:
+			//command=String("AT+B2400");
+			break;
+	}
 };
 bool HC12::changePower(unsigned int in){
+	//TODO: FIX  sendCommand(char * ptr, unsigned int length);
 };
 bool HC12::changeChannel(unsigned int in){
+	String temp = String("AT+C" + in);
+	sendCommand(temp);
+};
+unsigned short HC12::stageChange(bool resetVal){
+	if(!resetVal)
+		return newStage;
+	unsigned short temp = newStage;
+	newStage=0;
+	return temp;
 };
--- libraries/HC12/HC12.hpp
+++ libraries/HC12/HC12.hpp
@@ -1,5 +1,19 @@
+#include "Arduino.h"
+#define HC12H
 	
 class HC12{
+	public:
+	HC12(HardwareSerial*, unsigned short);
+	bool init();
+	bool send(String);
+	unsigned int receive();
+	bool changeBaud(unsigned int);
+	bool changePower(unsigned int);
+	bool changeChannel(unsigned int);
+	unsigned short stageChange(bool);
+	unsigned long long lastHeartbeat;
+	String lastRead;
+	
 	private:
 	const unsigned short commandPin;
 	unsigned int power;
@@ -7,21 +21,12 @@ class HC12{
 	unsigned int baud;
 	char SerialByteIn;                              // Temporary variable
 	char HC12ByteIn;                                // Temporary variable
-	String HC12ReadBuffer = "";                     // Read/Write Buffer 1 for HC12
-	String SerialReadBuffer = "";                   // Read/Write Buffer 2 for Serial
-	boolean SerialEnd = false;                      // Flag to indicate End of Serial String
-	boolean HC12End = false;                        // Flag to indiacte End of HC12 String
-	boolean commandMode = false;                    // Send AT commands
-	const Stream * serial;
-	void sendCommand(char *, unsigned int);
-	
-	public:
-	HC12(Stream *, unsigned short);
-	bool init();
-	bool send(char *);
-	unsigned int receive();
-	bool changeBaud(unsigned int);
-	bool changePower(unsigned int);
-	bool changeChannel(unsigned int);
-	
+	String HC12ReadBuffer;                     // Read/Write Buffer 1 for HC12
+	String SerialReadBuffer;                   // Read/Write Buffer 2 for Serial
+	boolean SerialEnd;                      // Flag to indicate End of Serial String
+	boolean HC12End;                        // Flag to indiacte End of HC12 String
+	boolean commandMode;                    // Send AT commands
+	unsigned short newStage;
+	HardwareSerial* serial;
+	void sendCommand(String);
 };
