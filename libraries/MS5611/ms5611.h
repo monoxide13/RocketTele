72d748e7dd25f4cc27bfb71af5148a0d14043e95
monoxide13 <git@monoxide13.com>
2019-11-06 23:00:00 -0600

    Updated MS5611 source. Changed SPI to use 16Mhz. Device rated to 20Mhz. Shortened some delays for MCU to pull chip select low. Added some sexy functions, and redid some to allow choosing resolution. Corrected some of the formulas that didn't match up with datasheet. May add some functions back that allow use without specifying specific resolution for compatibility. Included a schematic I found since I removed the 5v level shifting chip and associated resistors. I left voltage regulator since that is fed off the lipo. Cleaned up some other parts of the code.

--- libraries/MS5611/ms5611.h
+++ libraries/MS5611/ms5611.h
@@ -4,7 +4,7 @@
  *  Created on: Jun 26, 2018
  *      Author: kbisland
  */
-//#define DEBUGSERIAL
+#define DEBUGSERIAL
 
 #ifndef SRC_MS5611_H_
 #define SRC_MS5611_H_
@@ -21,27 +21,34 @@
 #define CMD_ADC_2048 0x06 // ADC OSR=2056
 #define CMD_ADC_4096 0x08 // ADC OSR=4096
 #define CMD_PROM_RD 0xA0 // Prom read command
+#define MS5611_SPI_CLOCK 16000000 // SPI clock speed
 
 
-class ms5611 {
+class MS5611 {
 private:
 	int _cs;
 	unsigned int C[8]; // calibration coefficients
 	unsigned char n_crc; // crc value of the prom
-
+	unsigned char resetComplete;
+	double stationPressure;
 public:
-	ms5611(int);
+	MS5611(int);
 	unsigned char init();
-	unsigned long cmd_adc(char cmd) ;
-	unsigned long getPressure();
-	unsigned long getTemperature();
+	unsigned long cmd_adc(char) ;
+	unsigned long getPressure(char);
+	unsigned long getTemperature(char);
 	void cmd_reset();
 	unsigned int cmd_prom(char coef_num);
 	unsigned char crc4(unsigned int n_prom[]);
-	double getPressureCompensated();
-	double getTemperatureCompensated();
-
-
+	double getTemperatureCompensated(char);
+	double getPressureCompensated(char);
+	double getSecondOrderTemperatureCompensated(char);
+	double setStationPressure();
+	double getAltitude();
+	double getAltitude(char);
+	double convertMBtoMeters(double);
+	double convertMBtoFt(double);
+	double convertCtoF(double);
 };
 
 #endif /* SRC_MS5611_H_ */
