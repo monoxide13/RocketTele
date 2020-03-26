edba6e00e0720295163cf16db71c1ce7d7120bdd
monoxide13 <git@monoxide13.com>
2020-03-26 03:06:06 -0500

    Cleaned up main code.

--- Telemetry_Rocket/Telemetry_Rocket.ino
+++ Telemetry_Rocket/Telemetry_Rocket.ino
@@ -3,15 +3,15 @@
    For use on the AdaFruit Feather Logger.
 */
 
-#define SD_CS_PIN 4           // SPI Chip select
+#define SD_CS_PIN 4           // SD Chip select
 #define SD_CD_PIN 7           //  SD Card Detect
 #define SYSTEM_LED_PIN 8      // External LED indicating status.
 #define STAGESEPERATION_PIN 5  // Limit switch detects stage seperation
-#define HC12_SET_PIN 6        // HC12 serial TX/RX command mode pin
+#define HC12_SET_PIN 5        // HC12 serial TX/RX command mode pin
 #define MPU6050_INT_PIN 12    // Accel interrupt
 #define MPU6050_ADDRESS 0x68  // Accel I2C address
 #define MPU6511_CS 6          // Barometer chip select
-#define BUZZER_PIN            // Buzzer
+#define BUZZER_PIN 13            // Buzzer. 13 on board LED
 
 
 #include "global.hpp"
@@ -20,7 +20,7 @@
 #include <Wire.h>
 #include <wiring_private.h> // for pinPeripheral().
 #include <SPI.h>
-#ifdef USE_SD
+#ifdef OUTPUT_SD
 #include <SD.h>
 #endif
 #ifdef USE_TELE
@@ -30,11 +30,14 @@
 #include <helper_3dmath.h>
 #include <MPU6050.h>
 #include <MS5611.h>
+#include <NMEAGPS.h>
+#include <GPSport.h>
+
 
 #define TELE_TX //
 #define TELE_SU
 #define TELE_ST
-#ifdef USE_USB
+#ifdef OUTPUT_USB
 #undef TELE_SU
 #define TELE_SU(X) Serial.print(X);
 #undef TELE_TX
@@ -57,38 +60,44 @@ struct SenserStruct {
 } sensors;
 
 uint8_t stage = 0;
-bool sdPresent = false;
+bool sdOpen = false;
 bool telePresent = false;
 bool buzzerState = false;
+unsigned long buzzerChangeTime = 0;
 Quaternion accel_q;           // [w, x, y, z]         quaternion container
 VectorInt16 accel_a;         // [x, y, z]            accel sensor measurements
 int16_t accel_history[] = {0, 0, 0};
 uint8_t accel_hist_c = 0;
 uint8_t accel_buffer[64]; // FIFO storage buffer
-uint8_t buzzerCounter = 0;
+uint8_t buzzerCount = 0;
 volatile bool accelDataReady = false;     // indicates whether MPU interrupt pin has gone high
-unsigned int stageCounter = 1; // Start at 1 to keep all stages consistant
+unsigned int stageLoopCount = 1; // Start at 1 to keep all stages consistant
 uint32_t loopCount = 0;
+unsigned long loopTime = 0;
 unsigned long updateTime = 0;
 #ifdef USE_TELE
 HC12 telemetry(&Serial1, HC12_SET_PIN);
 #endif
 MPU6050 accel(MPU6050_ADDRESS);
-MS5611 barometer(BARO_CS);
-#ifdef USE_SD
+MS5611 barometer(MPU6511_CS);
+#ifdef OUTPUT_SD
 File logFile;
 #endif
+NMEAGPS gps;
+gps_fix gpsFix;
 
-Uart GPSSerial(&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
-void SERCOM1_Handler()
-{
-  GPSSerial.IrqHandler();
-}
+/* Defined in GPSPort.hf
+  Uart gpsPort(&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
+  void SERCOM1_Handler()
+  {
+  gpsPort.IrqHandler();
+  } */
 
 void accelInterrupt() {
   accelDataReady = true;
 }
 
+//***********  Setup  ************//
 void setup() {
   // Start I2C bus
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -99,7 +108,7 @@ void setup() {
 #endif
 
   // Configure Input/Output ports.
-#ifdef USE_USB
+#ifdef OUTPUT_USB
   Serial.begin(115200);
   while (!Serial.available()) {};
   Serial.println("-Starting...");
@@ -109,10 +118,10 @@ void setup() {
   pinMode(SD_CS_PIN, OUTPUT);
   digitalWrite(SD_CS_PIN, HIGH);
   pinMode(MPU6050_INT_PIN, INPUT);
-  pinMode(BARO_CS, OUTPUT);
+  pinMode(MPU6511_CS, OUTPUT);
 
   // Start serial ports.
-  GPSSerial.begin(115200);
+  gpsPort.begin(9600);
   pinPeripheral(10, PIO_SERCOM); // TX
   pinPeripheral(11, PIO_SERCOM); // RX
 
@@ -132,9 +141,8 @@ void setup() {
   // TODO: Turn buzzer off.
 
   // Check if SD Card is present.
-#ifdef USE_SD
-  sdPresent = digitalRead(SD_CD_PIN);
-  if (sdPresent) {
+#ifdef OUTPUT_SD
+  if (digitalRead(SD_CD_PIN)) {
 #ifdef USE_TELE
     TELE_TX("S33S33");
 #endif
@@ -146,104 +154,98 @@ void setup() {
 #ifdef USE_TELE
       TELE_TX("S34S34");
 #endif
+#ifdef USE_USB
+      Serial.println("Opening SD");
+#endif
       sensors.sd = 4;
       unsigned short trialCount = 0;
       char fileName[] = "FILENAME_PREFIX000";
       do {
         if (!SD.exists(fileName)) {
-          sprintf(fileName, "FILENAME_PREFIX%03d", trialCount);
+          sprintf(fileName, "FILENAME_PREFIX%03d\n", trialCount);
         }
       } while (logFile == NULL);
-      logFile = SD.open(fileName);
+      logFile = SD.open(fileName, FILE_WRITE);
+      if (logFile != NULL) {
 #ifdef USE_TELE
-      TELE_TX("S35S35");
+        TELE_TX("S35S35");
 #endif
 #ifdef USE_USB
-      Serial.print("-File opened as: ");
-      Serial.println(fileName);
+        Serial.print("-File opened as: ");
+        Serial.println(fileName);
 #endif
-      sensors.sd = 5;
-      logFile.write("File Opened");
-    } else {
+        sensors.sd = 5;
+        logFile.write("File Opened");
+        sdOpen = true;
+      } else {
 #ifdef USE_USB
-      Serial.println("Unable to open SD card!");
+        Serial.println("Error opening SD file!");
 #endif
-      sensors.sd = 1;
+        sensors.sd = 6;
 #ifdef USE_TELE
-      TELE_TX("S36S36");
+        TELE_TX("S36S36");
 #endif
-      sdPresent = false;
-    }
-  } else {
+      }
+    } else { // SD.begin()
+#ifdef USE_TELE
+      TELE_TX("S36S36"); // Send SD not present.
+#endif
+#ifdef OUTPUT_USB
+      Serial.println("-SD Card not able to start.");
+#endif
+      sensors.sd = 6;
+    } // end SD.begin
+  } else { // if SD not present
 #ifdef USE_TELE
     TELE_TX("S31S31"); // Send SD not present.
 #endif
-#ifdef USE_USB
-    Serial.println("-SD Card not present!");
+#ifdef OUTPUT_USB
+    Serial.println("-SD Card not present.");
 #endif
     sensors.sd = 1;
-  }
-#else
-  sensors.sd = 1;
+  } // end SD present
+#else // If SD card not used
+#ifdef USE_TELE
+  TELE_TX("S31S31"); // Send SD not present/used.
 #endif
-  if (sensors.sd == 5) {
-    // TODO: Sound buzzer two long tones.
-  } else {
-    // TODO: Sound buzzer two short tones.
-  }
+#ifdef OUTPUT_USB
+  Serial.println("-SD Card disabled.");
+#endif
+#endif // End SD card init
   delay(1000);
-  stageCounter = 1;
+  stageLoopCount = 1;
   stage = 0;
-  buzzerCounter = 0;
+  buzzerCount = 0;
 }
 
-
-
+//***********  Loop  ************//
 void loop() {
+  loopTime = millis();
   switch (stage) {
     case 0:
-#ifdef USE_TELE
-      if (stageCounter == 1) {
-        TELE_TX("C00C00");
-      }
-#endif
-#ifdef USE_USB
-      if (stageCounter == 1) {
+      if (stageLoopCount == 1) {
+#ifdef OUTPUT_USB
         Serial.println("-Stage 0 start");
-      }
 #endif
-      if (stageCounter == 1) {
-        updateTime = 500 + millis();
-        // TODO: Signal system startup. 3 short beeps.
+#ifdef USE_TELE
+        TELE_TX("C00C00");
+#endif
+        updateTime = 0;
+        buzzerCount = 0;
       }
-      if (updateTime < millis()) {
-        switch (buzzerCounter) {
-          case 0:
-            // TODO: Turn buzzer off.
-            updateTime = millis() + 500;
-            buzzerCounter = 1;
-            break;
-          case 1:
-            // TODO: Turn buzzer on.
-            updateTime = millis() + 500;
-            buzzerCounter = 2;
-            break;
-          case 2:
-            // TODO: Turn buzzer off.
-            updateTime = millis() + 500;
-            buzzerCounter = 3;
-            break;
-          case 3:
-            // TODO: Turn buzzer on.
-            updateTime = millis() + 500;
-            buzzerCounter = 4;
-            break;
-          case 4:
-            // TODO: Turn buzzer off.
-            stage = 1;
-            stageCounter = 0;
-            buzzerCounter = 0;
-            break;
+      if (updateTime <= loopTime) {
+        if (buzzerState) {
+          buzzerState = false;
+          digitalWrite(BUZZER_PIN, LOW);
+        } else {
+          buzzerState = true;
+          digitalWrite(BUZZER_PIN, HIGH);
+          ++buzzerCount;
+        }
+        updateTime = loopTime + 500;
+        if (buzzerCount >= INITDELAY) {
+          stage = 1;
+          stageLoopCount = 0;
         }
       }
       break;
@@ -251,8 +253,8 @@ void loop() {
        Preflight.
     */
     case 1:
-      if (stageCounter == 1) {
-#ifdef USE_USB
+      if (stageLoopCount == 1) {
+#ifdef OUTPUT_USB
         Serial.println("-Stage 1 start");
 #endif
 
@@ -263,7 +265,7 @@ void loop() {
       }
       if (millis() > updateTime) {
         stage = 2;
-        stageCounter = 0;
+        stageLoopCount = 0;
       }
       break;
     /* Case 2
@@ -281,10 +283,10 @@ void loop() {
     */
     case 2:
       static unsigned short sensorCount;
-      if (stageCounter == 1) {
+      if (stageLoopCount == 1) {
         sensorCount = 0;
         sensors.barometer = sensors.accel = sensors.gps = 0;
-#ifdef USE_USB
+#ifdef OUTPUT_USB
         Serial.println("-Stage 2 start");
 #endif
 #ifdef USE_TELE
@@ -302,7 +304,7 @@ void loop() {
 #ifdef USE_TELE
           TELE_TX(sensorChar);
 #endif
-#ifdef USE_USB
+#ifdef OUTPUT_USB
           Serial.println(sensorChar);
 #endif
         }
@@ -311,44 +313,50 @@ void loop() {
              ((sensors.gps - 1) % 4 == 0) && ((sensors.sd - 1) % 4 == 0)) {
           Serial.println("Sensors ready");
           stage = 3;
-          stageCounter = 0;
-          sensorCount=5; // Sensors are ready, skip the case loop;
+          stageLoopCount = 0;
+          sensorCount = 5; // Sensors are ready, skip the case loop;
         }
-        if (stageCounter > 100) {
-#ifdef USE_USB
+        if (stageLoopCount > 100) {
+#ifdef OUTPUT_USB
           Serial.println("ERROR! Sensors not sure if ready, moving forwards anyways.");
 #endif
 #ifdef USE_TELE
           TELE_TX("C39C39");
 #endif
           stage = 3;
-          stageCounter = 0;
-          sensorCount=5; // Sensors are ready, skip the case loop;
+          stageLoopCount = 0;
+          sensorCount = 5; // Sensors are ready, skip the case loop;
         }
       }
       switch (sensorCount) {
         case 0:
           switch (sensors.barometer) {
             case 0: // Reset sensor and set to startup.
-              barometer.cmd_reboot(); // Trigger reboot.
+              barometer.cmd_reset(); // Trigger reboot.
               sensors.barometer = 3;
               break;
             case 3: // Sensor in startup.
-              if (barometer.init()==0) {
+              if (barometer.init() == 0) {
                 sensors.barometer = 1; // Error.
+#ifdef OUTPUT_USB
+                Serial.println("Error starting barometer");
+#endif
               } else {
                 sensors.barometer = 4;
               }
               break;
             case 4: // Sensor in calibration. Set ground level.
               barometer.setStationPressure();
-            break;
+              break;
           }
           break;
         case 1:
           switch (sensors.accel) {
             case 0: // Check if sensor is present;
               sensors.accel = ( accel.testConnection() ? 3 : 1 ); // Check if sensor can be reached. If true set to startup '3';
+#ifdef OUTPUT_USB
+              Serial.println("Unable to start accel");
+#endif
               break;
             case 3: // Start sensor and init
               accel.initialize();
@@ -370,60 +378,74 @@ void loop() {
           switch (sensors.gps) {
             // TODO: IMPLEMENT GPS
             case 0:
-              /* Check if sensor is present, if so 3, if not 1. */
-              sensors.gps = 3;
-              sensors.gps = 1;
+              if (gpsFix.valid.status) {
+                sensors.gps = 3;
+#ifdef OUTPUT_USB
+                Serial.println("Valid GPS data found.");
+#endif
+              }
+              else
+              {
+                sensors.gps = 1;
+#ifdef OUTPUT_USB
+                Serial.println("No valid GPS data found. Not using.");
+#endif
+              }
               break;
             case 3:
               /* Check if getting valid packets from GPS. If so 4 */
+              if (gpsFix.status == gps_fix::STATUS_STD) {
+                sensors.gps = 4;
+#ifdef OUTPUT_USB
+                Serial.println("GPS fix status standard");
+#endif
+              }
               break;
             case 4:
               /* Looking for GPS. If locked on set 5. */
+              if (gpsFix.valid.location) {
+                sensors.gps = 5;
+#ifdef OUTPUT_USB
+                Serial.println("GPS Location found:");
+                Serial.printf("%l, %l\nAlt: %f, SatsInUse:%d\n",
+                              gpsFix.latitudeL(), gpsFix.longitudeL(), gpsFix.altitude(), gpsFix.satellites);
+#endif
+              }
               break;
           }
           break;
-          default:
+        default:
           break;
       }
       if (++sensorCount > 3) {
         sensorCount = 0;
       }
+      ++stageLoopCount;
       break;
     /* Case 3
-    *  Rocket should be ready. Varify all checks are complete.
-    *  If not send error and send back to sensor phase or reset.
+      Rocket should be ready. Varify all checks are complete.
+      If not send error and send back to sensor phase or reset.
     */
     case 3:
-      if (stageCounter == 1) {
-#ifdef USE_USB
-        Serial.println("-Stage 3 starting");
+      if (stageLoopCount == 1) {
+#ifdef OUTPUT_USB
+        Serial.println(" - Stage 3 starting");
 #endif
       }
-#ifdef USE_TELE
-      if (updateTime + 3000 < millis()) {
-        updateTime = millis();
-        TELE_TX("C30C30"); //  Waiting for go/no-go.
-      }
-      if (telemetry.lastRead.equals("RGORGO")) {
-        stageCounter = 0;
-        stage = 4;
-      }
-#else
-      if (stageCounter == 1000) {
+      if (stageLoopCount == 1000) {
         delay(5);
-        stageCounter = 0;
+        stageLoopCount = 0;
         stage = 4;
       }
-#endif
       break;
     /* Case 4
-       Rocket has been approved for launch. It polls only accel to detect launch.
+      Rocket has been approved for launch. It polls only accel to detect launch.
     */
     case 4:
-      if (stageCounter == 1) {
+      if (stageLoopCount == 1) {
         // Turn on accel interrupt.
-#ifdef USE_USB
-        Serial.println("-Stage 4 starting");
+#ifdef OUTPUT_USB
+        Serial.println(" - Stage 4 starting");
 #endif
         accel_hist_c = 0;
         // Clear accel overflow flag and empty buffer since we've been ignoring the interrupt.
@@ -437,9 +459,11 @@ void loop() {
       }
       if (updateTime <= millis()) {
         buzzerState = !buzzerState;
-#ifdef USE_SD
-        logFile.print("t:");
-        logFile.println(millis());
+#ifdef OUTPUT_SD
+        if (sdOpen) {
+          logFile.print("t: ");
+          logFile.println(millis());
+        }
 #else
         updateTime = millis() + 1000;
 #endif
@@ -450,9 +474,9 @@ void loop() {
         accel.getFIFOBytes(accel_buffer, accel.dmpGetFIFOPacketSize());
         accel.dmpGetQuaternion(&accel_q, accel_buffer);
         accel.dmpGetAccel(&accel_a, accel_buffer);
-#ifdef USE_SD
-        if (sdPresent) {
-          logFile.print("a:\t");
+#ifdef OUTPUT_SD
+        if (sdOpen) {
+          logFile.print("a: \t");
           logFile.print(accel_q.w);
           logFile.print("\t");
           logFile.print(accel_q.x);
@@ -468,7 +492,7 @@ void loop() {
           logFile.println(accel_a.z);
         }
 #endif
-#ifdef USE_USB
+#ifdef OUTPUT_USB
         Serial.print("Y Accel: ");
         Serial.println((float)accel_a.y / 8196);
 #endif
@@ -477,15 +501,15 @@ void loop() {
           accel_hist_c = 0;
         if ((accel_history[0] + accel_history[1] + accel_history[2]) / 3 <= -12294) {
           stage = 5;
-          stageCounter = 0;
+          stageLoopCount = 0;
         }
-#ifdef USE_USB
+#ifdef OUTPUT_USB
         Serial.println((accel_history[0] + accel_history[1] + accel_history[2]) / 3);
 #endif
       }
       break;
     /* Case 5
-       Rocket is on ascent. Log pressure, temp, accel, GPS, Transmit only altitude.
+      Rocket is on ascent. Log pressure, temp, accel, GPS, Transmit only altitude.
     */
     case 5:
       while (1) {
@@ -495,9 +519,9 @@ void loop() {
           accel.getFIFOBytes(accel_buffer, accel.dmpGetFIFOPacketSize());
           accel.dmpGetQuaternion(&accel_q, accel_buffer);
           accel.dmpGetAccel(&accel_a, accel_buffer);
-#ifdef USE_SD
-          if (sdPresent) {
-            logFile.print("a:\t");
+#ifdef OUTPUT_SD
+          if (sdOpen) {
+            logFile.print("a: \t");
             logFile.print(accel_q.w);
             logFile.print("\t");
             logFile.print(accel_q.x);
@@ -513,7 +537,7 @@ void loop() {
             logFile.println(accel_a.z);
           }
 #endif
-#ifdef USE_USB
+#ifdef OUTPUT_USB
           Serial.print("Y Accel: ");
           Serial.println(accel_a.y);
 #endif
@@ -524,13 +548,18 @@ void loop() {
       };
       break;
     /* Case 6
-       Rocket is on descent. Log pressure, temp, GPS. Transmit altitude and GPS.
+      Rocket is on descent. Log pressure, temp, GPS. Transmit altitude and GPS.
     */
     case 6:
       break;
       /*
-         Rocket has landed. Finish writes to SD card and close. Transmit GPS position.
+        Rocket has landed. Finish writes to SD card and close. Transmit GPS position.
       */
   } // End switch(stage)
-  ++stageCounter;
+  while (gps.available(gpsPort)) {
+    gpsFix = gps.read();
+    Serial.print("GPS DATA: ");
+    Serial.println(gpsFix.valid.status);
+  }
+  ++stageLoopCount;
 } // End loop
--- Telemetry_Rocket/global.hpp
+++ Telemetry_Rocket/global.hpp
@@ -1,7 +1,7 @@
 // Global header file
-#pragma once
 
 #define FILENAME_PREFIX LAUNCH_
-//#define USE_TELE
-#define USE_SD
-#define USE_USB
+#define USE_TELE
+#define OUTPUT_SD
+#define OUTPUT_USB
+#define INITDELAY 30 // Delay in seconds after turning on before starting initializing sensors.
