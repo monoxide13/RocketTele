6c8405b5b269a366f4ef86de6da10cac06efd4db
monoxide13 <git@monoxide13.com>
2020-07-30 02:47:49 -0500

    Removed old files

--- Telemetry_Rocket/global.hpp
+++
@@ -1,7 +0,0 @@
-// Global header file
-
-#define FILENAME_PREFIX LAUNCH_
-#define USE_TELE
-#define OUTPUT_SD
-#define OUTPUT_USB
-#define INITDELAY 30 // Delay in seconds after turning on before starting initializing sensors.
