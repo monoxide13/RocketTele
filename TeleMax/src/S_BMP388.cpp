3d5cbb0b286d263067b3d98ef34300fe4cdb0a71
monoxide13 <git@monoxide13.com>
2021-10-01 00:43:58 -0500

    Added BMP388 sensor files.

---
+++ TeleMax/src/S_BMP388.cpp
@@ -0,0 +1,99 @@
+///// S_BMP388.cpp /////
+
+#include "S_BMP388.hpp"
+#include "Logging.hpp"
+#include "Staging.hpp"
+#include "TeleMax.hpp"
+
+
+S_BMP388::S_BMP388() {
+	unsigned long counter=0;
+	Logging::log(2, "-BMP388 in startup.\n");
+	S_BMP388_int::dataReady=false;
+	baro=new BMP388_DEV(BMP388_CS_PIN);
+	temp=pres=altitude=0;
+	// If sensor is not connected or wrong ID, begin() returns 0. Success=1.
+	if(baro->begin(SLEEP_MODE)){ //Set mode to forced/one shot. 0x01
+		sensorStatus=2; // Sensor good, in start up.
+		Logging::log(2, "-BMP388 Startup complete.\n");
+	}else{
+		sensorStatus=1; // Sensor no good.
+		Logging::log(1, "-BMP388 unable to startup.\n");
+	}
+};
+
+S_BMP388::~S_BMP388(){
+	delete (baro);
+};
+
+short S_BMP388::initialize(){
+	if(sensorStatus==1){
+		Logging::log(1, "-BMP388 in error. Unable to initialize.\n");
+		return -1;
+	}
+	Logging::log(3, "-BMP388 starting calibration.\n");
+	// Take 10 readings, and average
+	int x;
+	temp=pres=0;
+	altitude=0;
+	baro->setPresOversampling(OVERSAMPLING_X32);
+	baro->setTempOversampling(OVERSAMPLING_X2);
+	baro->setIIRFilter(IIR_FILTER_OFF);
+	for(x=0; x<5; ++x){
+		baro->startForcedConversion();
+		float tTemp, tPres, tAlt;
+		delay(100);
+		if(!(baro->getMeasurements(tTemp, tPres, tAlt))){
+			--x;
+			continue;
+		}
+		temp += tTemp;
+		pres += tPres;
+		altitude += tAlt;
+		delay(200);
+	}
+	temp = round(temp/10);
+	pres = pres/10;
+	Logging::log(2, "-BMP388 reporting temperature: " + String(temp) + "c\n");
+	Logging::log(2, "-BMP388 station pressure:" + String(pres) + "mbar\n");
+	Logging::log(2, "-BMP388 starting altitude:" + String(altitude) + "m\n");
+	// Change to normal settings, enable interrupt
+	baro->setPresOversampling(OVERSAMPLING_X2);
+	baro->setTempOversampling(OVERSAMPLING_SKIP);
+	baro->setIIRFilter(IIR_FILTER_OFF);
+	baro->setTimeStandby(TIME_STANDBY_40MS);
+	attachInterrupt(digitalPinToInterrupt(BMP388_INT_PIN), S_BMP388_int::callback, RISING);
+	baro->enableInterrupt(PUSH_PULL, ACTIVE_HIGH, UNLATCHED);
+	baro->startNormalConversion();
+	sensorStatus=0;
+	return 0;
+};
+
+void S_BMP388::tick(){
+	if(sensorStatus!=0)
+		return;
+	if(S_BMP388_int::dataReady){
+		++counter;
+		// TODO: Read measurement.
+		if(!(baro->getMeasurements(temp, pres, altitude))){
+			Logging::log(2, "-BMP388. Reading ready but unable to read");
+		}
+		Logging::log(1, "B:" + String(altitude) + ',' + String(temp) + ',' + String(pres) + '\n');
+		S_BMP388_int::dataReady=false;
+	}
+
+};
+
+double S_BMP388::getMeasurement(){
+	if(sensorStatus!=0)
+		return 0;
+	Logging::log(3, "-B: measurements taken: " + String(counter) + '\n');
+	Logging::telemetryData->data.balt = altitude;
+	counter=0;
+	return 0;
+};
+
+volatile bool S_BMP388_int::dataReady;
+void S_BMP388_int::callback(){
+	S_BMP388_int::dataReady=true;
+};
---
+++ TeleMax/src/S_BMP388.hpp
@@ -0,0 +1,30 @@
+//// S_BMP388.hpp /////
+
+#pragma once
+
+#include "Sensor.hpp"
+#include "BMP388_DEV.h"
+#include "global.hpp"
+
+#define STATUS_OFFSET 4
+
+class S_BMP388 : public virtual Sensor{
+	public:
+	S_BMP388();
+	~S_BMP388();
+	short initialize() override;
+	void tick() override;
+	double getMeasurement() override;
+	private:
+	BMP388_DEV* baro;
+	float altitude;
+	float temp;
+	float pres;
+	unsigned long counter;
+};
+
+// Interrupt Handling
+namespace S_BMP388_int{
+	void callback();
+	extern volatile bool dataReady;
+};
